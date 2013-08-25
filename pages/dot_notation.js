6c9b13aba9201cd2f1b4d86a3bcbfee430095e83
bogdan <onikiienko@gmail.com>
2013-08-25 19:26:20 +0300

    using dot notation

---
+++ pages/dot_notation.js
@@ -0,0 +1,20 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
+	if(err) throw err;
+
+	var query = { 'media.oembed.type' : 'video' };
+	var projection = {'media.oembed.url' : 1, '_id' : 0};
+
+	var cursor = db.collection('reddit').find(query, projection);
+
+	cursor.each(function(err, doc) {
+		if(err) throw err;
+
+		if(doc == null) {
+			return db.close();
+		}
+		console.dir(doc);
+		//console.dir(doc.student + " got a good grade! The grade is " + doc.grade);
+	});
+});
