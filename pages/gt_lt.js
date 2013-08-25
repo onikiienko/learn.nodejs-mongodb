773f3f9b35b7737f2906bd2adad870c6984ed8c4
bogdan <onikiienko@gmail.com>
2013-08-25 17:53:32 +0300

    example of using  and  in nodejs

---
+++ pages/gt_lt.js
@@ -0,0 +1,39 @@
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/course', function(err, db) {
+	if(err) throw err;
+
+	var query = {'grade' : {$gte : 50, $lte : 80}};
+	var projection = {'student' : 1, 'grade' : 1, '_id' : 0};
+
+	/*
+	//USING FINDONE
+
+	db.collection('grades').findOne(query, function(err, doc) {
+		if(err) throw err;
+
+		console.dir(doc);
+
+		db.close();
+	});
+	
+	// USING FIND
+	db.collection('grades').find(query).toArray(function(err, docs) {
+		if(err) throw err;
+
+		console.dir(docs);
+
+		db.close();
+	});*/
+	var cursor = db.collection('grades').find(query, projection);
+
+	cursor.each(function(err, doc) {
+		if(err) throw err;
+
+		if(doc == null) {
+			return db.close();
+		}
+		console.dir(doc);
+		console.dir(doc.student + " got a good grade! The grade is " + doc.grade);
+	});
+});
